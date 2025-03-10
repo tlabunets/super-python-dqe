5f016b60dae947be34a0ebbcb7dab2f45b9c9ef1
Tetiana Kravchuk <tetianakravchuk@unknowna6924c3250f8.lan>
2025-03-10 21:00:28 +0000

    add new file

---
+++ PythonProject/3.String.py
@@ -0,0 +1,8 @@
+'''tHis iz your homeWork, copy these Text to variable.
+
+You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+
+it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+
+last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
+'''
