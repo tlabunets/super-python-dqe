400754475301b22fd7dab7f5b6a11937ced48234
tlabunets <tetiana.labunets@gmail.com>
2025-03-11 15:39:18 +0000

    add string

--- PythonProject/3.String.py
+++ PythonProject/3.String.py
@@ -1,8 +1,34 @@
-'''tHis iz your homeWork, copy these Text to variable.
+import re
+# text into variable
+text = '''tHis iz your homeWork, copy these Text to variable.\n\n
+You NEED TO normalize it fROM letter CASEs point oF View. also, 
+create one MORE senTENCE witH LAST WoRDS of each existING SENtence 
+and add it to the END OF this Paragraph.\n\n
+it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.\n\n
+last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, 
+not only Spaces, but ALL whitespaces. I got 87.'''
 
-You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+# 1. Normalize text
+text = text.lower()
 
-it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+# 2. Replace "iz" to "is", if "iz" is not in quotes
+text = re.sub(r'(?<![“"])iz(?![”"])', 'is', text)
 
-last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
-'''
+#split text to sentences
+sentences = re.split(r'([.!?])', text)
+
+words = []
+for i in range(len(sentences)):
+    sentences[i] = sentences[i].strip().capitalize() #capitalize every sentence
+    word = sentences[i].split() #split every sentence to words
+   # print(word[-1])
+    if not word:
+        continue
+    if word[-1] != '.':
+       words.append(word[-1]) #take last word from every sentence and add into separate sentence
+text = ' '.join(sentences)
+print(text)
+print(' '.join(words).capitalize())
+
+whitespace_count = len(re.findall(r'\s', text))
+print('Number of whitespaces is ', whitespace_count)
